40a828782795ce7bc582c72cb0590d1d57243c71
Dmytro Sayko <saykodmytro@icloud.com>
2023-12-03 12:32:46 +0200

    up

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,15 @@
+import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
+import { addTodo } from 'redux/todosSlice';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export const SearchForm = ({ onSubmit }) => {
+export const SearchForm = () => {
   const [query, setQuery] = useState('');
+
+  const dispatch = useDispatch();
 
   const handleInput = e => {
     setQuery(e.currentTarget.value);
@@ -13,7 +18,12 @@ export const SearchForm = ({ onSubmit }) => {
   const handleSubmit = e => {
     e.preventDefault();
 
-    onSubmit(query);
+    const todo = {
+      id: nanoid(),
+      text: query,
+    };
+
+    dispatch(addTodo(todo));
 
     setQuery('');
   };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import 'modern-normalize';
 
 import { App } from 'components';
 import { GlobalStyles, theme } from 'styles';
+import { persistor, store } from 'redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles} />
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
